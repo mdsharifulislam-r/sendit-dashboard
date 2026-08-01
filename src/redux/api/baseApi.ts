56eee8e3953bcb0dd309d6c52dd3d95803518912
Shariful Islam <eng.mdshariful.islam.7@gmail.com>
2026-08-01 17:28:23 +0600

    change the region

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,9 @@ const token = typeof window !== "undefined" ? localStorage.getItem("token") : nu
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://10.10.26.164:3010/api/v1",
+        // baseUrl: "http://10.10.26.164:3010/api/v1",
+        baseUrl: "https://shariful5000.binarybards.online/api/v1",
+
         prepareHeaders: (headers) => {
             const currentToken = typeof window !== "undefined" ? localStorage.getItem("token") : token;
             if (currentToken) {
@@ -20,4 +22,7 @@ export const api = createApi({
 
 // export const imageUrl = "http://206.189.231.81:5000";
 // export const imageUrl = "http://10.10.7.72:5000";
-export const imageUrl = "http://10.10.26.164:3010";
+// export const imageUrl = "http://10.10.26.164:3010";
+export const imageUrl = "https://shariful5000.binarybards.online";
+
+
